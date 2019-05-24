ba3ed041c7d997bf9d048d64cdf20fd798b09f96
Daniel Emod Kovacs <kovacsemod@gmail.com>
2019-05-25 00:24:51 +0200

    feature(build): enable tree-shaking for smaller bundle size

--- scripts/generateReact.js
+++ scripts/generateReact.js
@@ -1,8 +1,15 @@
 const svgr = require('@svgr/core').default;
 const fs = require('fs');
-const { join, resolve } = require('path');
-const { promisify } = require('util');
-const { pascal } = require('change-case');
+const {
+	join,
+	resolve
+} = require('path');
+const {
+	promisify
+} = require('util');
+const {
+	pascal
+} = require('change-case');
 const mkdirp = require('mkdirp');
 
 const readFile = promisify(fs.readFile);
@@ -12,39 +19,58 @@ const mkdir = promisify(mkdirp);
 
 const iconDir = resolve(join(__dirname, '../icons'));
 
-const outDir = resolve(join(__dirname, '../src/generated'));
+const outDir = resolve(join(__dirname, '../src/icons'));
+
+const rootDir = resolve(join(__dirname, '../src'));
 
 const readIcon = async (fileName) => {
-    const content = await readFile(join(iconDir, fileName));
-    return { name: fileName, content };
+	const content = await readFile(join(iconDir, fileName));
+	return {
+		name: fileName,
+		content
+	};
 };
 
 const transformName = name => pascal(name.replace('.svg', ''));
 
-const transformIcon = ({name, content}) => svgr(content, svgrOptions, { componentName: transformName(name) })
+const transformIcon = ({
+	name,
+	content
+}) => svgr(content, svgrOptions, {
+	componentName: transformName(name)
+})
 
-const writeIcon = (name, fileContent) => writeFile(join(outDir, name), fileContent, { encoding: 'utf-8' });
+const writeIcon = (name, fileContent) => writeFile(join(outDir, name), fileContent, {
+	encoding: 'utf-8'
+});
 
-const template = (
-    { template, types },
-    opts,
-    { componentName, jsx, props }
-  ) => {
-    const typeScriptTpl = template.smart({ plugins: ['typescript', '@svgr/plugin-jsx'] })
-    
-    // We add these here, instead of svgrOptions.svgProps, because those are added after this function evaluates
-    // which means our spread property below will be overriden
-    const lineJoin = types.jsxAttribute(types.jsxIdentifier('strokeLinejoin'), types.stringLiteral('miter'));
-    const lineCap = types.jsxAttribute(types.jsxIdentifier('strokeLinecap'), types.stringLiteral('square'));
-    const width = types.jsxAttribute(types.jsxIdentifier('strokeWidth'), types.stringLiteral('2'));
-    jsx.openingElement.attributes.push(lineJoin);
-    jsx.openingElement.attributes.push(lineCap);
-    jsx.openingElement.attributes.push(width);
+const template = ({
+		template,
+		types
+	},
+	opts, {
+		componentName,
+		jsx,
+		props
+	}
+) => {
+	const typeScriptTpl = template.smart({
+		plugins: ['typescript', '@svgr/plugin-jsx']
+	})
 
-    const spreadExtraProps = types.jsxSpreadAttribute(types.identifier('extraProps'));
+	// We add these here, instead of svgrOptions.svgProps, because those are added after this function evaluates
+	// which means our spread property below will be overriden
+	const lineJoin = types.jsxAttribute(types.jsxIdentifier('strokeLinejoin'), types.stringLiteral('miter'));
+	const lineCap = types.jsxAttribute(types.jsxIdentifier('strokeLinecap'), types.stringLiteral('square'));
+	const width = types.jsxAttribute(types.jsxIdentifier('strokeWidth'), types.stringLiteral('2'));
+	jsx.openingElement.attributes.push(lineJoin);
+	jsx.openingElement.attributes.push(lineCap);
+	jsx.openingElement.attributes.push(width);
 
-    jsx.openingElement.attributes.push(spreadExtraProps);
-    return typeScriptTpl.ast`
+	const spreadExtraProps = types.jsxSpreadAttribute(types.identifier('extraProps'));
+
+	jsx.openingElement.attributes.push(spreadExtraProps);
+	return typeScriptTpl.ast `
     import * as React from 'react';
     import { transformContext, IkonateContext } from '../context';
     export const ${componentName} = React.forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props: React.SVGProps<SVGSVGElement>, svgRef: React.Ref<SVGSVGElement>) => {
@@ -58,28 +84,31 @@ const template = (
 }
 
 const svgrOptions = {
-    icon: true,
-    ref: true,
-    svgProps: {
-        stroke: 'currentColor',
-        fill: 'none'
-    },
-    template
+	icon: true,
+	ref: true,
+	svgProps: {
+		stroke: 'currentColor',
+		fill: 'none'
+	},
+	template
 };
 
 const createIcon = async (fileName) => {
-    const file = await readIcon(fileName);
-    const fileContent = await transformIcon(file);
-    const componentName = transformName(file.name);
-    await writeIcon(componentName + '.tsx', fileContent);
-    return `export * from './${componentName}';`;
+	const file = await readIcon(fileName);
+	const fileContent = await transformIcon(file);
+	const componentName = transformName(file.name);
+	await writeIcon(componentName + '.tsx', fileContent);
+	return `export { ${componentName} } from './icons/${componentName}';`;
 }
 
 async function main() {
-    await mkdir(outDir);
-    const icons = await readDir(iconDir);
-    const index = await Promise.all(icons.map(createIcon));
-    await writeFile(join(outDir, 'index.ts'), index.join('\n'), { encoding: 'utf-8' });
+	await mkdir(outDir);
+	const icons = await readDir(iconDir);
+	const index = await Promise.all(icons.map(createIcon));
+	index.push(`export { IkonateContext } from './context';`)
+	await writeFile(join(rootDir, 'index.ts'), index.join('\n'), {
+		encoding: 'utf-8'
+	});
 }
 
 main();
