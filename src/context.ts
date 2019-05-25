ac9770ef62c88a521d1049aa96812629372a035b
Daniel Emod Kovacs <kovacsemod@gmail.com>
2019-05-25 11:48:13 +0200

    fix(bundle): include multiple builds with tsc

--- src/context.ts
+++ src/context.ts
@@ -1,39 +1,46 @@
-import { createContext, SVGProps } from 'react';
+import * as React from 'react';
 
 export type IkonateContextType = {
-    border?: number;
-    size?: number | string;
-    style?: 'square' | 'roundSquare' | 'round';
-    color?: string;
-}
+	border?: number;
+	size?: number | string;
+	style?: 'square' | 'roundSquare' | 'round';
+	color?: string;
+};
 
-export const IkonateContext = createContext<IkonateContextType>({});
+export const IkonateContext = React.createContext<IkonateContextType>({});
 
 type IconStyle = {
-    strokeLinecap: 'square' | 'round';
-    strokeLinejoin: 'miter' | 'round';
-}
+	strokeLinecap: 'square' | 'round';
+	strokeLinejoin: 'miter' | 'round';
+};
 
 type StyleMap = {
-    square: IconStyle;
-    roundSquare: IconStyle;
-    round: IconStyle
-}
+	square: IconStyle;
+	roundSquare: IconStyle;
+	round: IconStyle;
+};
 
 const styleMap: StyleMap = {
-    square: { strokeLinecap: 'square', strokeLinejoin: 'miter' },
-    roundSquare: { strokeLinecap: 'round', strokeLinejoin: 'miter' },
-    round: { strokeLinecap: 'round', strokeLinejoin: 'round' }
-}
+	square: { strokeLinecap: 'square', strokeLinejoin: 'miter' },
+	roundSquare: { strokeLinecap: 'round', strokeLinejoin: 'miter' },
+	round: { strokeLinecap: 'round', strokeLinejoin: 'round' },
+};
 
-type ContextTransformer = (context: IkonateContextType) => SVGProps<SVGSVGElement>;
+type ContextTransformer = (
+	context: IkonateContextType,
+) => React.SVGProps<SVGSVGElement>;
 
-export const transformContext: ContextTransformer = ({ style = 'square', size, border = 2, color }) => {
-    const iconStyle = styleMap[style!];
-    return {
-        ...iconStyle,
-        strokeWidth: border,
-        color,
-        fontSize: size
-    }
-}
+export const transformContext: ContextTransformer = ({
+	style = 'square',
+	size,
+	border = 2,
+	color,
+}) => {
+	const iconStyle = styleMap[style!];
+	return {
+		...iconStyle,
+		strokeWidth: border,
+		color,
+		fontSize: size,
+	};
+};
